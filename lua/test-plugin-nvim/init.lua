34da5de38aeeb2d1bba89fef6aed1bd9695927e3
John Loyd Mulit <jlmulit68@gmail.com>
2024-03-28 21:20:18 +0800

    feat: some changes

--- lua/test-plugin-nvim/init.lua
+++ lua/test-plugin-nvim/init.lua
@@ -1,1 +1,8 @@
-print("hello from test-plugin-nvim")
+local function curr_location()
+    vim.ui.input({ prompt = "Dir name: " }, function(input)
+        local path = vim.fn.expand("%:p:h")
+        vim.cmd(path .. "/" .. input)
+    end)
+end
+
+curr_location()
